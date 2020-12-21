da673ebe237c7aea3ea358546f341c159ea02e8f
abcqwerty111 <47075954+abcqwerty111@users.noreply.github.com>
2020-12-21 11:57:04 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,9 +9,9 @@ def send_welcome(message):
 	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 	markup.add('Скинь документ')
 	bot.reply_to(message, 'Введите слово или словосочетание для поиска. Для получения документа с вопросами и ответами нажми "Скинь документ"', reply_markup=markup)
-	print(f'{message.chat.id}, {message.from_user.username} -> start')
+	print(f'{message.chat.id}, {message.from_user.first_name}, {message.from_user.username} -> start')
 	if message.chat.id != 888833912:
-		bot.send_message(888833912, f'{message.chat.id}, {message.from_user.username} -> start')
+		bot.send_message(888833912, f'{message.chat.id}, {message.from_user.first_name}, {message.from_user.username} -> start')
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
@@ -23,10 +23,10 @@ def echo_all(message):
 
 	if message.text == 'Скинь документ':
 		doc = open('stp_file.docx', 'rb')
-		print(f'{cid}, {message.from_user.username} -> document')
+		print(f'{cid}, {message.from_user.first_name}, {message.from_user.username} -> document')
 		bot.send_document(cid, doc, reply_markup=markup)
 		if cid != 888833912:
-			bot.send_message(888833912, f'{cid}, {message.from_user.username} -> document')
+			bot.send_message(888833912, f'{cid}, {message.from_user.first_name}, {message.from_user.username} -> document')
 
 	else:
 		con = sqlite3.connect('bot_db.sqlite3')
@@ -34,7 +34,7 @@ def echo_all(message):
 		for row in cur.execute('SELECT * FROM answers'):
 			if (message.text in str(row[1])) or (message.text.lower() in str(row[1])) or (message.text.upper() in str(row[1])) or (message.text.capitalize() in str(row[1])):
 				bot.send_message(cid, f'{row[1]}\n\n{row[2]}', reply_markup=markup)
-				print(f'{cid}, {message.from_user.username}: {row[1]} --> {row[2]}')
+				print(f'{cid}, {message.from_user.first_name}, {message.from_user.username}: {row[1]} --> {row[2]}')
 				if cid != 888833912:
-					bot.send_message(888833912, f'{cid}, {message.from_user.username}: {row[1]} --> {row[2]}')
+					bot.send_message(888833912, f'{cid}, {message.from_user.first_name}, {message.from_user.username}: {row[1]} --> {row[2]}')
 bot.polling()
